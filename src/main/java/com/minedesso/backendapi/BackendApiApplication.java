23437ff96bf0c21ab5fe306c42593b938c69c469
Alessio Lopes <alessio.lopes@adesso.de>
2026-04-05 21:24:33 +0200

    feat: fixed authentication via swagger ui

--- src/main/java/com/minedesso/backendapi/BackendApiApplication.java
+++ src/main/java/com/minedesso/backendapi/BackendApiApplication.java
@@ -3,8 +3,7 @@ package com.minedesso.backendapi;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.security.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -17,8 +16,19 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 )
 @SecurityScheme(
         name = "keycloak",
-        type = SecuritySchemeType.OPENIDCONNECT,
-        openIdConnectUrl = "http://localhost:8090/realms/minedesso/.well-known/openid-configuration"
+        type = SecuritySchemeType.OAUTH2,
+        scheme = "bearer",
+        flows = @OAuthFlows(
+                authorizationCode = @OAuthFlow(
+                        authorizationUrl = "${spring.security.oauth2.resourceserver.jwt.issuer-uri}/protocol/openid-connect/auth",
+                        tokenUrl = "${spring.security.oauth2.resourceserver.jwt.issuer-uri}/protocol/openid-connect/token",
+                        scopes = {
+                                @OAuthScope(name = "openid", description = "OpenID Connect scope"),
+                                @OAuthScope(name = "profile", description = "Access to user profile information"),
+                                @OAuthScope(name = "email", description = "Access to user email address")
+                        }
+                )
+        )
 )
 public class BackendApiApplication {
     public static void main(String[] args) {
